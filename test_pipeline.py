e14329eace6763ab11117193e8d51e8c131093c0
tcandal <t.candal@gmail.com>
2025-07-18 23:55:49 -0300

    Update test_pipeline.py

--- test_pipeline.py
+++ test_pipeline.py
@@ -34,6 +34,33 @@ def test_data_validation():
     assert not df.empty, "DataFrame should not be empty."
     assert df['label'].isin(["positivo","negativo"]).all(), "All text entries should be non-null."
 
-#def test_placeholder():
-    # Placeholder test to ensure pytest runs
-#    assert True
+def test_fairness_by_text_lengh():
+    model_path = "./model.joblib"
+    vectorizer_path = "./vectorizer.joblib"
+    data_path = "./data/tweets_limpo.csv"
+    assert os.path.exists("./model.joblib"), "Model file does not exist."
+    assert os.path.exists("./vectorizer.joblib"), "Vectorizer file does not exist."
+    assert os.path.exists("./data/tweets_limpo.csv"), "Data file does not exist."
+    model = joblib.load(model_path)
+    vectorizer = joblib.load(vectorizer_path)
+    df = pd.read_csv(data_path)
+    df['text_len'] = df['text'].apply(len)
+    df['len_category'] = pd.cut(df['text_len'], bins=[0, 40, 100, 200, 1000], labels=["curto","medio","grande","textao"])
+    vetor = vectorizer.transform(df['text'])
+    y_true = df['label']
+    y_pred = model.predict(vetor)
+
+    results = {}
+    for cat in df['len_category'].unique():
+        subset = df[df['len_category'] == cat]
+        if not subset.empty:
+            x_sub = vectorizer.transform(subset['text'])
+            y_sub_true = subset['label']
+            y_sub_pred = model.predict(x_sub)
+            acc = accuracy_score(y_sub_true, y_sub_pred)
+            results[str(cat)] = acc
+
+    acc_value = list(results.values())
+    max_diff = max(acc_value) - min(acc_value)
+    print(f"Accuracy by text length category: {results}")
+    assert max_diff > 0.2, f"Fairness test failed: accuracy difference between categories is too high. {max_diff:.2f}."
